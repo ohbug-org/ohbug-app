eead5e0d65af370d1087c0dc6c4bbd870295609a
chenyueban <jasonchan0527@gmail.com>
2021-09-16 17:58:32 +0800

    fix: 修复 DatePicker 报错

--- src/components/styles/DatePicker.tsx
+++ src/components/styles/DatePicker.tsx
@@ -3,6 +3,7 @@ import dayjsGenerateConfig from 'rc-picker/lib/generate/dayjs'
 import generatePicker from 'antd/es/date-picker/generatePicker'
 import 'antd/es/date-picker/style/index'
 
+// @ts-ignore
 const DatePicker = generatePicker<Dayjs>(dayjsGenerateConfig)
 
 export default DatePicker
